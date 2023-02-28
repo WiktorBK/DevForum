1186b44ee76ec5611fef51e80006b881133b5e4d
wiktor bozek <wiktorsebbozek@gmail.com>
2023-02-28 20:56:53 +0100

    Initial commit

---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
+
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, room_id):
+    room = Room.objects.get(id=room_id)
+
+    
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
