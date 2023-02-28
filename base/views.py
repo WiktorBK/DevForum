4f1f442d2d9dd1ef22f552bf464a842bd8c7f40e
wiktor bozek <wiktorsebbozek@gmail.com>
2023-02-28 21:28:44 +0100

    Add create room option
    new endpoint, new html file

--- base/views.py
+++ base/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Room
-
+from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
-
 def room(request, room_id):
     room = Room.objects.get(id=room_id)
-
-    
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+    
