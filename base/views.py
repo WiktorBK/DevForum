9ac414168ae9ba5f24e00061eeb0474459a96fda
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-02 14:09:18 +0100

    Create more searching params

--- base/views.py
+++ base/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(description__icontains=q) |
+        Q(name__icontains=q) 
+        
+        )
+    
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, room_id):
